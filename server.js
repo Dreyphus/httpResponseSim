933c5287e319c62f22e122938006e35689aabe5c
Dreyphus <152345892+Dreyphus@users.noreply.github.com>
2024-09-04 22:01:11 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -4,16 +4,25 @@ const theParser = require("body-parser")
 const app = express()
 
 app.use(cors())
-
+app.use(express.json());
 app.use(theParser.urlencoded({extended : true}))
+
+var reqCounter = 0
 
 app.get('/', (req, res) => {
     res.status(500)
     console.log(req.body)
 })
 app.post('/', (req, res) => {
-    res.status(500)
+    if (Object.keys(req.body).length === 0) {
+        reqCounter++  
+        console.log(reqCounter)
+        return res.status(400).send('Request body is empty');
+    }
+    res.status(200)
+    reqCounter++
     console.log(req.body)
+    console.log(reqCounter)
 })
 
 app.listen(3000)
