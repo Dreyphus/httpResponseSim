6fbbb70e241dd39c6a5e8d362c7aa580d917cbe6
Dreyphus <152345892+Dreyphus@users.noreply.github.com>
2024-09-05 20:28:02 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ app.use(cors())
 app.use(express.json());
 app.use(theParser.urlencoded({extended : true}))
 
-var reqCounter = 0
 
 app.get('/', (req, res) => {
     res.status(500)
@@ -15,14 +14,38 @@ app.get('/', (req, res) => {
 })
 app.post('/', (req, res) => {
     if (Object.keys(req.body).length === 0) {
-        reqCounter++  
-        console.log(reqCounter)
-        return res.status(400).send('Request body is empty');
+        console.log("Empty body")
     }
-    res.status(200)
-    reqCounter++
-    console.log(req.body)
-    console.log(reqCounter)
+    
+    const jConst = req.body["codes"]
+    console.log(jConst)
+    switch (jConst) {
+        case "200":
+            res.status(200).send("Success")
+            break;
+        case "301":
+            res.status(301).send("Not quite")
+            break;
+        case "307":
+            res.status(307).send("Not quite")
+            break;  
+        case "400":
+            res.status(400).send("Success")
+            break;
+        case "403":
+            res.status(403).send("Not quite")
+            break;
+        case "404":
+            res.status(404).send("Not quite")
+            break;           
+        case "500":
+            res.status(500).send("Not quite")
+            break;  
+    }
+//    if(jConst == "200"){
+//        console.log("In")
+//        res.status(200).send("Success")
+//    }
 })
 
 app.listen(3000)
